df0aab646fd5f9d7fc969b37b1be42f0231a65c6
tamiresataide <tamiresaataide@gmail.com>
2022-10-24 17:18:42 -0300

    funções

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-alert('Olá Mundo!')
 console.log('Olá Mundo 3.0')
 
 //Variáveis
@@ -22,3 +21,44 @@ console.log(typeof idade);
 console.log(typeof logado);
 console.log(typeof lista);
 console.log(typeof pessoa);
+
+// Condicional ternário
+
+let isMember = true;
+
+let shipping = isMember ? 2 : 10;
+
+console.log(isMember ? "Você é membro" : "Você NÃO é membro");
+console.log(`Frete: R$${shipping},00`);
+
+let age = 14;
+
+console.log((age >= 18) ? "Você é maior de idade" : "Você é menor de idade");
+
+//switch
+
+let profession = 'veterinário';
+
+switch (profession) {
+   case 'bombeiro':
+      console.log(`${profession}, sua camisa será VERMELHA`);
+      break;
+   case 'policial':
+      console.log(`${profession}, sua camisa será AZUL`);
+      break;
+   case 'fiscal':
+      console.log(`${profession}, sua camisa será VERDE`);
+      break;
+   default:
+      console.log(`${profession}, sua camisa será PRETA`);
+      break;
+}
+
+
+//funções
+
+function verificarIdade(idade){
+   return idade >= 18 ? true : false;
+}
+
+console.log(verificarIdade(25));
