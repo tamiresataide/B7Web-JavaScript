427d4f0a6acda3f011c5acc091912dcb0ecd2f16
tamiresataide <tamiresaataide@gmail.com>
2022-10-24 09:52:46 -0300

    tipos de dados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,23 @@
 alert('Olá Mundo!')
 console.log('Olá Mundo 3.0')
 
+//Variáveis
+
 let idade = 28;
 let nome = 'Tamires';
 let cidade = 'Campina Grande'
 let estado = 'Paraíba'
 
 console.log(nome + ' tem ' + idade + ' anos e mora em ' + cidade + ' na ' + estado);
+
+//Tipos de dados
+
+let logado = true;
+let lista = ['ovo', 'farinha', 'açúcar']
+let pessoa = {nome: 'Tamires', idade: 28}
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof logado)
+console.log(typeof lista)
+console.log(typeof pessoa)
