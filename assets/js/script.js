c2caa28d810d33cf29ebd13f66818979b769a339
tamiresataide <tamiresaataide@gmail.com>
2022-10-24 09:37:03 -0300

    variáveis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log('Olá JavaScript')
+alert('Olá Mundo!')
+console.log('Olá Mundo 3.0')
+
+let idade = 28;
+let nome = 'Tamires';
+let cidade = 'Campina Grande'
+let estado = 'Paraíba'
+
+console.log(nome + ' tem ' + idade + ' anos e mora em ' + cidade + ' na ' + estado);
