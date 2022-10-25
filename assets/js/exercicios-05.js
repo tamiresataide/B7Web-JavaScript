b77b315810f5d129f60b747d07826d94e4207a9a
tamiresataide <tamiresaataide@gmail.com>
2022-10-25 09:21:24 -0300

    Exercicio 05

---
+++ assets/js/exercicios-05.js
@@ -0,0 +1,15 @@
+/*
+Crie uma função que valide usuário e senha.
+Usuário correto: pedro
+Senha correta: 123
+*/
+
+//Uso da função:
+let usuario = 'bonieky';
+let senha = '1234';
+let validacao = validar(usuario, senha);
+if(validacao){
+   console.log('Acesso concedido.');
+} else {
+   console.log('Acesso NEGADO!');
+}
