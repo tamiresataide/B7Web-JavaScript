300219283f1713c21120851bc8cc44016ca2fe17
tamiresataide <tamiresaataide@gmail.com>
2022-10-25 09:12:21 -0300

    exercicio 04 - solução

--- assets/js/exercicios-04.js
+++ assets/js/exercicios-04.js
@@ -7,7 +7,38 @@ Calcule o preço do imóvel
 Uso da função:
 */
 
+function calcularImovel(metragem, quartos){
+   let metro2 = 3000
+   let preco = 0
+   switch (quartos) {
+      case 1:
+      default:
+         preco = (metragem * metro2).toFixed(2);
+         break;
+      case 2:
+         metro2 = metro2 * 1.2
+         preco = (metragem * metro2).toFixed(2);
+         break;
+      case 3:
+         metro2 = metro2 * 1.5
+         preco = (metragem * metro2).toFixed(2);
+         break;
+   }
+   return preco;
+}
+
 let metragem = 123;
 let quartos = 3;
 let preco =  calcularImovel(metragem, quartos);
-console.log(`O imóvel custa R$${preco} reais`)
+console.log(`O imóvel custa R$ ${preco} reais`)
+
+
+metragem = 30;
+quartos = 1;
+preco =  calcularImovel(metragem, quartos);
+console.log(`O imóvel custa R$ ${preco} reais`)
+
+metragem = 50;
+quartos = 2;
+preco =  calcularImovel(metragem, quartos);
+console.log(`O imóvel custa R$ ${preco} reais`)
