c90eb940bafb967ac36d582f4c092812f0a34e22
tamiresataide <tamiresaataide@gmail.com>
2022-10-25 11:30:39 -0300

    exercicios 06 - resolvidos

--- assets/js/exercicios-06.js
+++ assets/js/exercicios-06.js
@@ -1,18 +1,20 @@
 // 1. No array abaixo, qual o número que pega a Ferrari?
 let carros = ['BMW', 'Ferrari', 'Mercedes'];
-let x = 
-console.log(`1 ${carros[x]}`);
+let x = 1;
+console.log(`1. ${carros[x]}`);
 
 // 2. Troque Ferrari por Audi
 
+carros[1] = 'Audi'
 console.log ('2. Lista com Audi');
 console.log(carros);
 
 // 3. Adicione Volvo a lista
 
+carros.push('Volvo');
 console.log('3. Lista com Volvo');
 console.log(carros);
 
 // 4. Exiba quantos itens tem no array
 
-console.log('4. Itens no array: ');
+console.log(`4. Itens no array: ${carros.length}`);
