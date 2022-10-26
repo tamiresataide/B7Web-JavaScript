f1e8a6478160b31fd5458dfbd142e2d8d742fa83
tamiresataide <tamiresaataide@gmail.com>
2022-10-26 09:49:20 -0300

    exerciccios 07 - resolvidos

---
+++ assets/js/exercicios-07.js
@@ -0,0 +1,12 @@
+// 1. Faça um loop que mostre todas as frutas
+let fruits = ['Maçã', 'Uva', 'Banana'];
+for (let fruta of fruits) {
+   console.log(fruta)
+}
+
+// 2. Conte de 1 até 100 através de um WHILE
+let n = 1;
+while(n <= 100) {
+   console.log(n);
+   n++;
+}
